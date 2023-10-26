792ab35355400ed13465ff9bd235b00ad80f87db
Pathpat <xristospatakias@gmail.com>
2023-10-26 17:12:52 +0300

    Create a model for orders and declare the types in orders.graphql

---
+++ orders/orders.model.js
@@ -0,0 +1,16 @@
+module.exports = [
+    {
+        date: '2005-05-12',
+        subtotal: 90.12,
+        items: [
+            {
+                product: {
+                    id: 'redshoe',
+                    description: 'Old Red Shoe',
+                    price: 45.06,
+                },
+                quantity: 2,
+            }
+        ]
+    }
+];
