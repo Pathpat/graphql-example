6e0bf84eab1fcfc0f028b237e31597ec2376bd6e
Pathpat <xristospatakias@gmail.com>
2023-10-28 12:28:13 +0300

    Setting up  the resolvers in server.js

--- server.js
+++ server.js
@@ -4,28 +4,29 @@ const { graphqlHTTP } = require('express-graphql');
 
 const { loadFilesSync } = require('@graphql-tools/load-files');
 const { makeExecutableSchema } = require('@graphql-tools/schema');
+const { info } = require('console');
 
 //old fashioned way
 //const typesArray = loadFilesSync(path.join(__dirname, '**/*.graphql'))
 
 const typesArray = loadFilesSync('**/*', {
     extensions: ['graphql']
+});
+
+const resolversArray = loadFilesSync('**/*', {
+    extensions: ['resolvers.js']
 })
+
 
 const schema = makeExecutableSchema({
-    typeDefs: typesArray
-})
-
-const root = {
-    products: require('./products/products.model'),
-    orders: require('./orders/orders.model'),
-}
+    typeDefs: typesArray,
+    resolvers: resolversArray,
+});
 
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
     schema: schema,
-    rootValue: root,
     graphiql: true,
 }))
 
